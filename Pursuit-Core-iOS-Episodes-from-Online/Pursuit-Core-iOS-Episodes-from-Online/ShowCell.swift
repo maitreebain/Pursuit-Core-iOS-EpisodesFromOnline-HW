b8fdec709ee36044b91e2c836227fa60d6c57757
maitreebain <maitreebain@gmail.com>
2019-12-12 15:21:18 -0500

    working on show cell

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ShowCell.swift
@@ -0,0 +1,15 @@
+//
+//  ShowCell.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Maitree Bain on 12/12/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ShowCell: UITableViewCell {
+
+    
+
+}
--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ShowsViewController.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ShowsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ShowsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
