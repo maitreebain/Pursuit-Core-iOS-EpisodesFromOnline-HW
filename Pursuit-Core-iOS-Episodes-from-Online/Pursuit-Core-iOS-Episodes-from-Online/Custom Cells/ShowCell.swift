25e0c1215b486808e46d29991101615526bb8f19
maitreebain <maitreebain@gmail.com>
2019-12-13 17:04:32 -0500

    working on custom cell data

--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Custom Cells/ShowCell.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Custom Cells/ShowCell.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ShowsCell: UITableViewCell {
     
+    @IBOutlet weak var showImage: UIImageView!
     
-
+    @IBOutlet weak var showName: UILabel!
+    
+    
+    func configureCell(for shows: ShowsDataLoad) {
+        
+        showName.text = shows.show.name
+        
+        guard let url = URL(string: shows.image.medium) else {
+            print("no show url found")
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            
+            switch result{
+            case .failure(let appError):
+                print("appError: \(appError)")
+            case .success(let data):
+                
+            }
+        }
+    }
 }
--- Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/View Controllers/ShowsViewController.swift
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/View Controllers/ShowsViewController.swift
@@ -10,13 +10,44 @@ import UIKit
 
 class ShowsViewController: UIViewController {
 
+    @IBOutlet weak var showsTableView: UITableView!
     
+    var shows = [ShowsDataLoad](){
+        didSet {
+            DispatchQueue.main.async {
+                self.showsTableView.reloadData()
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        showsTableView.dataSource = self
+        showsTableView.delegate = self
     }
 
 
 }
 
+extension ShowsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 180
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "showsCell", for: indexPath) as? ShowsCell else {
+            fatalError("didn't conform to custom cell")
+            
+            let selectedShow = shows[indexPath.row]
+            
+            
+            
+            return cell
+        }
+    }
+}
